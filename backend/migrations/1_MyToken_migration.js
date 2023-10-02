714b475484ba1f85607dde5d80f4699558b94141
papiasharmin <papiasharmin33@gmail.com>
2023-10-02 18:12:09 +0900

    added .gitsubmodule

--- backend/migrations/1_MyToken_migration.js
+++
@@ -1,6 +0,0 @@
-const MyToken = artifacts.require("./MyToken");
-
-module.exports = async function (deployer) {
-  deployer.deploy(MyToken, "MyToken", "MTN");
-
-};
--- backend/migrations/2_FactoryV4_migration.js
+++
@@ -1,5 +0,0 @@
-const WalletFactory = artifacts.require("./WalletFactoryV4.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(WalletFactory);
-};
